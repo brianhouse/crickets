c7a37bb955ffcb1a786afda50ce0213e03470342
Brian House <email@brian.house>
2025-04-17 11:48:49 -0400

    config

--- cricket/config.py
+++ cricket/config.py
@@ -6,8 +6,8 @@ HOOD = 5
 # capacitor constants
 FREQ = 1            # endogenous frequency in Hz
 BUMP = 0.05         # capacitor bump amount when neighbor flashes
-REST = 0.2         # percent of cycle to be unresponsive after flash
-TICK = 20           # cycle resolution in ms
+REST = 0.2          # percent of cycle to be unresponsive after flash
+TICK = 10           # cycle resolution in ms
 
 # automatic constants
 PITCH_LOW, PITCH_HIGH = 3000, 4500
