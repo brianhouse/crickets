07494425fd7ef037ca9a3178b8393e57bb006e54
Brian House <23200+brianhouse@users.noreply.github.com>
2025-04-22 20:51:48 -0400

    timing improvements

--- cricket/config.py
+++ cricket/config.py
@@ -2,7 +2,7 @@
 POWER = 7
 MAX_HOOD = 7
 INIT_HOOD = 5
-MIN_HOOD = 3
+MIN_HOOD = 0
 FRIEND_LINK = .2
 GROUP_LEADER = .2
 SEVER = -3
@@ -11,7 +11,7 @@ SEVER = -3
 FREQ = 1            # endogenous frequency in Hz
 BUMP = 0.01         # capacitor bump amount when neighbor flashes
 REST = 0.10         # percent of cycle to be unresponsive after flash
-TICK = 10           # cycle resolution in ms
+TICK = 0.01         # cycle resolution in seconds
 
 # automatic constants
 PITCH_LOW, PITCH_HIGH = 3000, 4500
@@ -25,6 +25,7 @@ CHIRP = False
 
 
 from util import *
+
 
 
 
