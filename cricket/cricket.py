ce77034725cfba3c7d99845e7a5125921f31832e
Brian House <23200+brianhouse@users.noreply.github.com>
2025-06-06 15:25:30 -0400

    new attempt with trigger-only scheme

--- cricket/cricket.py
+++ cricket/cricket.py
@@ -17,23 +17,13 @@ class Cricket(Node):
         self.hum = randint(HUM_LOW, HUM_HIGH)
         self.t_previous = ticks_ms()
         self.peers = []
-        self.group = "null"
+        self.group = None
         self.paused = False
         O.print(f"## {self.name} ##")
 
     async def run(self):
         try:
             await asyncio.sleep_ms(randint(1000, 3000))
-            if HUM:
-                SND.duty(0)
-                SND.duty(512)
-                SND.freq(self.hum)
-            STS.on()
-            await self.look()
-            if not STATUS or (self.group != self.name):
-                STS.off()
-            if HUM:
-                SND.duty(0)
             self.receive()  # clear messages
         except Exception as e:
             if DEBUG:
@@ -63,23 +53,12 @@ class Cricket(Node):
                     if MOTION:
                         if PIR.value():
                             print("MOTION")
-                            if HUM:
-                                SND.duty(0)
-                                SND.duty(512)
-                                SND.freq(self.hum)
-                            STS.on()
-                            await asyncio.sleep_ms(2000)  # needs to be at least 2s for the PIR
-                            await asyncio.sleep_ms(randint(0, 1000))  # mess up the oscillator
                             await self.look()
-                            if self.group != self.name:
-                                STS.off()
-                            if HUM:
-                                SND.duty(0)
                             continue
-                    if len(self.peers) < MIN_HOOD:
+                    if self.group is not None and len(self.peers) < MIN_HOOD:
                         print("BELOW MIN")
-                        await self.look()
-                        continue
+                        self.clear_peers()
+                        self.group = None
                     self.listen()
                     if self.capacitor >= 1.0:
                         self.flash()
@@ -92,85 +71,55 @@ class Cricket(Node):
                     O.print(e)
 
     async def look(self):
-        self.group = "null"
-        if random() < GROUP_LEADER:
-            self.group = self.name
+        O.print("LEADER...")
+        self.group = self.name
+        if STATUS:
             STS.on()
-            O.print("LEADER")
-        O.print("LOOK...")
+        if HUM:
+            SND.duty(0)
+            SND.duty(512)
+            SND.freq(self.hum)
+        STS.on()
+        await asyncio.sleep_ms(2000)  # needs to be at least 2s for the PIR
+        await asyncio.sleep_ms(randint(0, 1000))  # mess up the oscillator
         self.clear_peers()
         neighbors = self.scan(RANGE, True)  # sorting
         if len(neighbors):
             for i in range(MAX_HOOD):
                 if i < len(neighbors):
                     self.add_peer(neighbors[i])
+        self.send_all(f"group {self.group} {"*".join([peer.name for peer in self.peers])}")
+        if not STATUS or (self.group != self.name):
+            STS.off()
+        if HUM:
+            SND.duty(0)
         O.print("--> DONE")
 
     def listen(self):
         for sender, message in self.receive():
             O.print("GOT", message, "from", sender)
             try:
-                kind, sender_group, friend_name = message.split(" ")
+                kind, sender_group, group_list = message.split(" ")
             except Exception as e:
-                print(f"BAD ({e}): \"{message}\"")
+                O.print(f"BAD ({e}): \"{message}\"")
                 continue
-
-            if kind == "reject":
-                print("REJECTED")
-                self.remove_peer(sender)
-                continue
-
-            # both unassigned
-            if self.group == "null" and sender_group == "null":
-                # bump anyway
-                self.bump()
-
-            # self assigned, sender unassigned
-            elif sender_group == "null":
-                # add the peer to try to get it in the group
-                # ...but don't bump until they're in the group
-                print("SENDER UNASSIGNED")
-                self.add_peer(sender)
-
-            # self unassigned, sender assigned
-            elif self.group == "null":
-                # join this group
-                self.add_peer(sender)
+            if kind == "group":
+                O.print("GROUP", sender_group)
                 self.group = sender_group
-                if self.group == self.name:
-                    STS.on()
-                else:
-                    STS.off()
-                print("GROUP", self.group)
-                self.bump()
-
-            # both have groups assigned
-            else:
-                if self.group == sender_group:
-                    furthest = self.get_furthest()
-                    friend = Peer.find(name=friend_name)
-                    if sender not in self.peers:
-                        if len(self.peers) < MAX_HOOD:
-                            print("LOW HOOD")
-                            self.add_peer(sender)
-                        elif furthest is not None and sender.rssi > furthest.rssi:
-                            # we're not rejecting, just limiting sends
-                            print("CLOSER")
-                            self.remove_peer(furthest)
-                            self.add_peer(sender)
-                    if friend_name != self.name and friend not in self.peers:
-                        if len(self.peers) < MAX_HOOD:
-                            print("ROOM FOR FRIEND")
-                            self.add_peer(friend)
-                        elif furthest is not None and friend.rssi is not None and friend.rssi > furthest.rssi:
-                            print("FRIEND IS CLOSER")
-                            self.remove_peer(furthest)
-                            self.add_peer(friend)
+                self.clear_peers()
+                self.add_peer(sender)
+                for name in group_list.split("*"):
+                    self.add_peer(Peer.find(name))
+            elif kind == "reject":
+                O.print("REJECTED")
+                self.remove_peer(sender)
+            elif kind == "flash":
+                if sender_group == self.group:
                     self.bump()
                 else:
-                    # no longer friends
+                    O.print("REJECT")
                     self.add_peer(sender)
-                    self.reject(sender)
+                    self.send(f"reject {self.group} NOP", sender)
                     self.remove_peer(sender)
 
     def add_peer(self, peer):
@@ -200,44 +149,38 @@ class Cricket(Node):
                 O.print(e)
         self.peers.clear()
 
-    def get_furthest(self):
-        furthest = None
-        for peer in self.peers:
-            if peer.rssi is not None and (furthest is None or peer.rssi < furthest.rssi):
-                furthest = peer
-        return furthest
-
-    def send(self):
+    def send_all(self, message):
         O.print("SEND", self.peers)
         if len(self.peers):
-            super().send(f"flash {self.group} {choice(self.peers).name}")
+            super().send(message)
 
-    def reject(self, peer):
-        O.print(f"REJECT {peer}")
-        super().send(f"reject {self.group} NOP", peer)
+    def send(self, peer, message):
+        O.print(f"SEND {peer}")
+        super().send(message, peer)
 
     def flash(self):
         O.print("FLASH")
         self.phase = 0.0
         self.capacitor = 0.0
-        if BLINK:
-            LED.on()
-        else:
-            STS.on()
-        self.send()
-        if CHIRP:
-            SND.duty(512)
-            SND.freq(self.pitch * 2)
-        sleep_ms(30)
-        if CHIRP:
-            SND.freq(self.pitch)
-        sleep_ms(30)
-        if CHIRP:
-            SND.duty(0)
-        if BLINK:
-            LED.off()
-        else:
-            STS.off()
+        if self.group is not None:
+            if BLINK:
+                LED.on()
+            else:
+                STS.on()
+            self.send_all(f"flash {self.group} NOP")
+            if CHIRP:
+                SND.duty(512)
+                SND.freq(self.pitch * 2)
+            sleep_ms(30)
+            if CHIRP:
+                SND.freq(self.pitch)
+            sleep_ms(30)
+            if CHIRP:
+                SND.duty(0)
+            if BLINK:
+                LED.off()
+            else:
+                STS.off()
 
     def bump(self):
         if self.phase * 1000 < REST:
