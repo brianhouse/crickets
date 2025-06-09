2d9a7038f23e1b01ebae4468a1543129f13777a0
Brian House <email@brian.house>
2025-06-09 13:06:54 -0400

    install

--- upload.py
+++ upload.py
@@ -9,7 +9,7 @@ port = sys.argv[1]
 
 # run(f"mpremote connect {port} fs ls")
 for filename in os.listdir("cricket/"):
-    if filename.split(".")[-1] != "py":
+    if filename.split(".")[-1] not in ["py", "json"]:
         continue
     run(f"venv/bin/mpremote connect {port} cp cricket/{filename} :{filename}")
     print()
